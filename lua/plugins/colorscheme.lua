01a6b2309651781259e529e27a1f84200d6fcf7e
itsryuk <dip.end.raw101@gmail.com>
2026-07-22 06:14:10 -0400

    toggleterm added

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
-
 -- colorschemes
 
 vim.pack.add({
-    "https://github.com/rebelot/kanagawa.nvim",
-    "https://github.com/folke/tokyonight.nvim"
+  "https://github.com/rebelot/kanagawa.nvim",
+  "https://github.com/folke/tokyonight.nvim"
 })
 
 -- transparency config
@@ -11,6 +10,32 @@ vim.pack.add({
 require('kanagawa').setup({
   ...,
   transparent = true,
+  overrides = function(colors)
+    local theme = colors.theme
+    return {
+      -- Normal Float
+      NormalFloat = { bg = "none" },
+      FloatBorder = { bg = "none" },
+      FloatTitle = { bg = "none" },
+
+      -- Save an hlgroup with dark background and dimmed foreground
+      -- so that you can use it where your still want darker windows.
+      -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+      NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+      -- Popular plugins that open floats will link to NormalFloat by default;
+      -- set their background accordingly if you wish to keep them dark and borderless
+      LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+      MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+      -- Pmenu
+      Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+      PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+      PmenuSbar = { bg = theme.ui.bg_m1 },
+      PmenuThumb = { bg = theme.ui.bg_p2 },
+    }
+  end,
+
   colors = {
     theme = {
       all = {
@@ -21,6 +46,15 @@ require('kanagawa').setup({
     }
   },
 })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "kanagawa",
+  callback = function()
+    vim.api.nvim_set_hl(0, "ToggleTermNormal", { bg = "#1F1F28" })
+    vim.api.nvim_set_hl(0, "ToggleTermNormalNC", { bg = "#16161d" })
+  end,
+})
+
 vim.cmd('colorscheme kanagawa')
-
-
+vim.cmd('highlight TelescopeBorder guibg = none')
+vim.cmd('highlight TelescopeTitle guibg = none')
